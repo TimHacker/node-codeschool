0dda5e4433e31a152ec6494aa05cd63c583fd7a6
Tim Hacker <tim.hacker@trueclarity.co.uk>
2013-09-20 22:12:59 +0100

    basic app to check heroku works with that

--- server.js
+++ server.js
@@ -1,41 +1,54 @@
-var express = require('express');
+// var express = require('express');
+// var app = express();
+
+// var server = require('http').createServer(app);
+// var io = require('socket.io').listen(server);
+
+// server.listen(8080);
+
+// app.set('view engine', 'ejs');
+
+// app.get('/', function(req, res) {
+//     res.sendfile(__dirname + "/index.html");
+// });
+
+// io.configure(function() {
+//     io.set("transports", ["xhr-polling"]);
+//     io.set("polling duration", 10);
+// });
+
+// io.sockets.on('connection', function(socket) {
+//     socket.on('join', function(name) {
+//         socket.set('nickname', name);
+//         console.log(name + " joined.");
+//     });
+
+//     console.log('Client connected...');
+
+//     socket.on('messages', function(message) {
+
+//         socket.get('nickname', function(err, name) {
+//             socket.broadcast.emit('messages', '<-- ' + name + ': ' + message);
+//             console.log(name + " sent " + message);
+//         });
+
+//     });
+// });
+
+
+// //app.listen(8080);
+
+// console.log("Server has started on port 8080");
+
+var express = require("express");
 var app = express();
+app.use(express.logger());
 
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-server.listen(8080);
-
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + "/index.html");
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-io.configure(function() {
-    io.set("transports", ["xhr-polling"]);
-    io.set("polling duration", 10);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-io.sockets.on('connection', function(socket) {
-    socket.on('join', function(name) {
-        socket.set('nickname', name);
-        console.log(name + " joined.");
-    });
-
-    console.log('Client connected...');
-
-    socket.on('messages', function(message) {
-
-        socket.get('nickname', function(err, name) {
-            socket.broadcast.emit('messages', '<-- ' + name + ': ' + message);
-            console.log(name + " sent " + message);
-        });
-
-    });
-});
-
-
-//app.listen(8080);
-
-console.log("Server has started on port 8080");
