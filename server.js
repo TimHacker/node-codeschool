1f113a57c144d3ff5345f46fa594ef8e94b436c0
Tim Hacker <tim.hacker@trueclarity.co.uk>
2013-09-20 17:54:50 +0100

    very basic socket io

--- server.js
+++ server.js
@@ -1,51 +1,26 @@
 var express = require('express');
-var request = require('request');
-var url = require('url');
-var util = require('util');
-var twitterAPI = require('node-twitter-api');
-var ejs = require('ejs');
-
-var twitter = new twitterAPI({
-    consumerKey: '1bqgKCYYc3apD7tdIbGXMg',
-    consumerSecret: 'Jkt9XQmb5oRIChqZMK4zgP3vJ3c5OhSPaWtgLtTEQ',
-    callback: 'http://yoururl.tld/something'
-});
-
-var access_token_key = '28795209-eFflFcXhNci65Z7DZkcQS4Y94cEiOwoee5vU7vQKr';
-var access_token_secret = 'koJQHYbLcXR7V5UlqTl1h2lP59lCuKZaMFf12tCw';
-
 var app = express();
-    app.set('view engine', 'ejs');
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.set('view engine', 'ejs');
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendfile(__dirname + "/index.html");
 });
 
-app.get('/tweets/:username', function(req, res) {
-    var username = req.params.username;
-    //res.write("Most recent tweet for " + username + "\n");
-
-    twitter.getTimeline('user', {
-        screen_name: username,
-        count: 10
-    }, access_token_key, access_token_secret, function(err, data) {
-
-        if (data !== undefined) {
-            res.render('tweets', {
-                tweets: data,
-                name: username
-            });
-        }
-
-        if (err !== null) {
-            res.end(err);
-        }
-    });
-
-
+io.sockets.on('connection', function (socket) {
+    console.log('Client connected...');
+    // socket.emit('news', {hello: 'world'});
+    // socket.on('my other event', function (data) {
+    //     console.log(data);
+    // });
 });
 
-app.listen(8080);
+//app.listen(8080);
 
 console.log("Server has started on port 8080");
